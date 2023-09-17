82dcb14b9e55cd935367f35b86c9504bef800415
mrvam <marian.mrva@outlook.com>
2023-09-17 15:19:18 +0200

    add code

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Posts from 'components/Posts'
+
+function Home() {
+  return (
+    <div >
+      <h1 className='header'>Welcome to blog app</h1>
+      <Posts />
+    </div>
+  )
+}
+
+export default Home;
---
+++ pages/posts/[id].js
@@ -0,0 +1,42 @@
+import { getPost } from 'api/posts';
+import { dehydrate, QueryClient } from '@tanstack/react-query';
+import { withCSR } from 'HOC/with-CSR'
+import Error from 'components/Error';
+import PostPage from 'containers/PostPage';
+
+const Page = ({ isError }) => {
+
+    //show custom error component if there is an error
+    if (isError) return <Error />
+
+    return <PostPage />
+
+}
+
+export const getServerSideProps = withCSR(async (ctx) => {
+
+    console.log('getServerSideProps');
+
+    const { id } = ctx.params;
+
+    const queryClient = new QueryClient();
+
+    let isError = false;
+
+    try {
+        await queryClient.fetchQuery(['post', id], () => getPost(id));
+    } catch (error) {
+        isError = true
+        ctx.res.statusCode = error.response.status;
+    }
+
+    return {
+        props: {
+            //also passing down isError state to show a custom error component.
+            isError,
+            dehydratedState: dehydrate(queryClient),
+        },
+    }
+})
+
+export default Page;
